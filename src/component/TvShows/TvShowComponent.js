3dc0e715b3eb36b39ce6b1f2e9ca7b435b2c5de4
BenHampton <benhampton11@yahoo.com>
2018-10-30 00:17:42 -0500

    Updated css and tvShow similar movies wont render if there aren't any to render

--- src/component/TvShows/TvShowComponent.js
+++ src/component/TvShows/TvShowComponent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import '../../../node_modules/primereact/components/fieldset/Fieldset.css'
 import {TVServices} from "../../services/TVServices";
 import TvShowView from "./TvShowView";
+import {MediaService} from "../../services/MediaService";
 
 class TvShowComponent extends Component{
     constructor(props){
@@ -10,9 +11,11 @@ class TvShowComponent extends Component{
             tvShow: this.props.location.state.tvShow,
             trailer: this.props.location.state.tvShowsTrailerKey,
             tvShowReviews: [],
+            similarMedia: [],
             hasReviews: true
         }
         this.getMovieDB = new TVServices();
+        this.getSimilarMovieDB = new MediaService();
         this.refreshTvShowPage = this.refreshTvShowPage.bind(this);
     }
 
@@ -22,6 +25,7 @@ class TvShowComponent extends Component{
 
     componentDidMount(){
         this.tvShowReviews = this.getMovieDB.getTVShowReviews(this, this.state.tvShow.id);
+        this.similarMedia = this.getSimilarMovieDB.getSimilarMedia(this, this.state.tvShow.id);
     }
 
     refreshTvShowPage(similarMedia){
@@ -38,6 +42,7 @@ class TvShowComponent extends Component{
                          tvShowReviews={this.state.tvShowReviews}
                          hasReviews={this.state.hasReviews}
                          refreshTvShowPage={this.refreshTvShowPage}
+                        similarMedia={this.state.similarMedia}
             />
         )
     }
